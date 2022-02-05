bcf8effc99e1416a31af1cc6f3b38183f3eaffbd
karen <karenayman98@gmail>
2022-02-05 19:13:11 +0200

    lab1

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('store');
+});
+Route::get('/home', [IndexController::class, 'home']);
+Route::get('/product/{id}', [IndexController::class, 'product_details']);
